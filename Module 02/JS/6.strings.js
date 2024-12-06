48333872e6e78bc19607513e095a84b618fce02f
Poojan Dave <poojandave0506@gmail.com>
2024-12-06 11:20:40 +0530

    commit for lec 60

---
+++ Module 02/JS/6.strings.js
@@ -0,0 +1,16 @@
+// this is lec 60
+let a ="PoojanDaveDave"
+let b =" Love with lana"
+console.log(a[0])
+console.log(a[6])
+
+console.log(a.length)
+console.log("hello, " + a)
+console.log(`hello, ${a}`)
+
+console.log(a.toUpperCase())
+console.log(a.toLowerCase())
+console.log(a.slice(1,4))
+console.log(a.replace("oo","00"))
+console.log(a.replace("Dave"," ")) // double occurance, first will be replaced
+console.log(a.concat(" is in",b))
