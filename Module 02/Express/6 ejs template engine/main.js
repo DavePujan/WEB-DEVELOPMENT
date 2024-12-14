9196526cb707ffcae06db6726431323807b78d5a
Poojan Dave <poojandave0506@gmail.com>
2024-12-14 12:35:44 +0530

    commit for lec 92

---
+++ Module 02/Express/6 ejs template engine/main.js
@@ -0,0 +1,27 @@
+// this is lec 92
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    let siteName = "Adidas"
+    let searchText = "Search Now"
+    let arr = ["hello1","hello2","hello3"]
+  res.render("index.ejs",{siteName : siteName, search : searchText, arr}) // here you can give name or you can also change...
+//   or 
+// res.render("views/index.html",{siteName,searchText})
+})
+
+app.get('/blog/:slug', (req, res) => {
+    let blogTitle = "adidas wht and when?"
+    let blogContent = "its a very nice brand"
+
+  res.render("blogpost.ejs",{blogContent : blogContent, blogTitle : blogTitle}) 
+  });
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
