5e6618a085e889ca944657855fbc3f8fc87e4d4f
Poojan Dave <poojandave0506@gmail.com>
2025-02-01 15:58:02 +0530

    commit for lec 95

--- Module 02/MongoDB/2.crud.mongodb.js
+++ Module 02/MongoDB/2.crud.mongodb.js
@@ -66,15 +66,15 @@ use('crudDatabase');
 // // Delete operation
 // db.courses.deleteMany({price: 1000});
 
-Operator    Description:
-$eq         Matches values that are equal to a specified value.
-$gt         Matches values that are greater than a specified value.
-$gte        Matches values that are greater than or equal to a specified value.
-$in         Matches any of the values specified in an array.
-$lt         Matches values that are less than a specified value.
-$lte        Matches values that are less than or equal to a specified value.
-$ne         Matches all values that are not equal to a specified value.
-$nin        Matches none of the values specified in an array.
+// Operator    Description:
+// $eq         Matches values that are equal to a specified value.
+// $gt         Matches values that are greater than a specified value.
+// $gte        Matches values that are greater than or equal to a specified value.
+// $in         Matches any of the values specified in an array.
+// $lt         Matches values that are less than a specified value.
+// $lte        Matches values that are less than or equal to a specified value.
+// $ne         Matches all values that are not equal to a specified value.
+// $nin        Matches none of the values specified in an array.
 
 // $eq Example:
 db.courses.find({price: {$eq: 0}});
