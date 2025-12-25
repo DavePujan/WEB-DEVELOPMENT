c41866888a360e34e71c3d080e015b6a76524fec
Poojan Dave <poojandave0506@gmail.com>
2025-12-25 15:49:20 +0530

    React Shreyians Basics

---
+++ Module 04/React Shreyians/01-folder-setup/src/App.jsx
@@ -0,0 +1,18 @@
+const App = () => {
+
+  return (
+    <> {/*wrapper*/}
+    <div id="parent1">
+      <div className="kid1">Kid 1</div>
+      <div className="kid2">Kid 2</div>
+    </div>
+    <div id="parent2">
+      <div className="kid1">Kid 1</div>
+      <div className="kid2">Kid 2</div>
+    </div>
+    </>
+  )
+
+}
+
+export default App
